239d15d0b07da5d88911a3197eddf9bdc02fb600
Jimmy <caoqiguang@gmail.com>
2018-06-05 19:24:39 +0800

    shadowsocks

--- shadowsocks.sh
+++ shadowsocks.sh
@@ -69,7 +69,8 @@ EOF
 chmod 754 /usr/lib/systemd/system/shadowsocks.service
 systemctl enable shadowsocks
 systemctl start shadowsocks
-
+firewall-cmd --add-port=$PORT1/tcp --permanent
+firewall-cmd --add-port=$PORT2/tcp --permanent
 echo -e "You can now connect to your Shadowsocks via your external IP \033[32m${ip2}\033[0m"
 echo -e "Port1: \033[32m${PORT1}\033[0m" "Password: \033[32m${PWD1}\033[0m"
 echo -e "Port2: \033[32m${PORT2}\033[0m" "Password: \033[32m${PWD2}\033[0m"
