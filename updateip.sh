ce5c4dd3a83fc6afde1f7112316e94ecf58c3517
Jimmy <caoqiguang@gmail.com>
2020-11-01 14:06:55 +0800

    bash

--- updateip.sh
+++ updateip.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
-Wan_IP=`curl ipv4.icanhazip.com`
+Wan_IP=`curl -s https://api.ip.sb/ip`
 Last_IP=`cat /home/Last_IP.txt`
-if [ "$Wan_IP" != "$Last_IP" ]
+if [[ $Wan_IP =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]] && [ "$Wan_IP" != "$Last_IP" ]
 then
 echo $Wan_IP >/home/Last_IP.txt
 echo "WAN IP changed, send out alerts..."
-mail -s "Your New Wlan IP" 111@gmail.com < /home/Last_IP.txt
+mail -s "Your New Wlan IP" test@gmail.com < /home/Last_IP.txt
 else
 
         echo "Your IP Not changed."
-
 fi
-
