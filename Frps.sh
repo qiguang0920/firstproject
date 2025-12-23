cce5982261906bda1bf2d1f3b700112e64d864e2
JIMMY <caoqiguang@gmail.com>
2025-12-23 14:50:27 +0800

    Frps

--- Frps.sh
+++ Frps.sh
@@ -2,8 +2,7 @@
 # Install Frp_server for CentOS/Rocky/AlmaLinux
 
 # Check if user is root
-[ $(id -u) != "0" ] && { echo -e "\033[31mError: You must be root to run this script\033[0m"; exit 1; } 
-
+[ $(id -u) != "0" ] && { echo -e "\033[31mError: This script must be run as root!\033[0m"; exit 1; } 
 export PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 clear
 printf "
@@ -12,28 +11,38 @@ printf "
 #            More information http://www.iewb.net                     #
 #######################################################################
 "
+# 定义一个函数来等待任意按键
+wait_for_key() {
+    echo -n "Press any key to start...."
+    # -n 1: 只读取一个字符
+    # -s: 不回显用户输入
+    read -n 1 -s key
+    echo "" # 换行，使后续输出整齐
+}
+wait_for_key
 pwd=`pwd`
-wget https://github.com/fatedier/frp/releases/download/v0.65.0/frp_0.65.0_linux_amd64.tar.gz
 yum install tar -y 
+wget https://github.com/fatedier/frp/releases/download/v0.65.0/frp_0.65.0_linux_amd64.tar.gz
 tar -zxvf frp_0.65.0_linux_amd64.tar.gz 
 #下载是否完成
 if [ ! -e "$pwd/frp_0.65.0_linux_amd64/frps" ]; then echo "Download frp From Github failed"; exit 1; fi
+mv frp_0.65.0_linux_amd64 Frp_server
 rm -rf frp_0.65.0_linux_amd64.tar.gz
 
-dashboard_user="admin"
-dashboard_pwd="admin"
 token="IEWB.NET_$RANDOM"
 while :; do echo
-    read -p "Please input dashboard username: " dashboard_user
+    read -t 20 -p "Please input dashboard username: " dashboard_user
+	dashboard_user=${dashboard_user:-admin}
     [ -n "$dashboard_user" ] && break
 done
 
 while :; do echo
-    read -p "Please input dashboard password: " dashboard_pwd
+    read -t 20 -p "Please input dashboard password: " dashboard_pwd
+	dashboard_pwd=${dashboard_pwd:-admin}
     [ -n "$dashboard_pwd" ] && break
 done
 
-cat > $pwd/frp_0.65.0_linux_amd64/frps.ini <<EOF
+cat > $pwd/Frp_server/frps.ini <<EOF
 #通用设置
 [common]
 # frp 监听地址
@@ -63,7 +72,7 @@ firewall-cmd --reload
 touch /home/frp.sh
 	cat > /home/frp.sh <<EOF
 #!/bin/bash
-$pwd/frp_0.65.0_linux_amd64/frps -c $pwd/frp_0.65.0_linux_amd64/frps.ini &
+$pwd/Frp_server/frps -c $pwd/Frp_server/frps.ini &
 EOF
 
 chmod +x /home/frp.sh
